7a0ca301b6b32dfaea8cf81e5f88a19232bf5b66
Safin Ahmed <safin.ahmed@tacerto.com>
2017-08-09 21:43:13 +0100

    Changed app logic. Few changes on design. Added export as bookmarks feature

--- js/popup.js
+++ js/popup.js
@@ -1,107 +1,122 @@
-if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number] 
-        : match
-      ;
-    });
-  };
-}
-
-var winCounter=99999;
+var winCounter = 99999; //Used for new windows, so IDs don't clash
+var windowTemplate;
+var bookmarksTemplate;
+var allWindows;
 
 $(document).ready(function() {
-	winCounter = 99999;
-	chrome.windows.getAll({"populate":true},processWindows);
-	$("#saveBtn").click(SaveChanges);
-	$("#newWinBtn").click(newWin);
+    chrome.windows.getAll({ "populate": true }, processWindows);
+    $("#saveBtn").click(saveChanges);
+    $("#newWinBtn").click(newWin);
+
+    var windowSource = $("#window-template").html();
+    windowTemplate = Handlebars.compile(windowSource);
+
+    var bookmarksSource = $("#bookmarks-template").html();
+    bookmarksTemplate = Handlebars.compile(bookmarksSource);
 });
 
 function processWindows(windows) {
-	var results = $("#results").empty();	
-	windows.forEach(function(v) { 
-		addWindow(v,results);
-	});
-	
-	$(".tabli").sortable({
-	  connectWith: ".connected"
-	});
-	
-	$("li").tooltip({
-        placement : 'top'
-    });	
+    allWindows = [];
+    var results = $("#results").empty();
+
+    windows.forEach(function(v) {
+        allWindows.push({ id: v.id, tabs: v.tabs });
+        addWindow(v, results);
+    });
+
+    $(".tabli").sortable({
+        connectWith: ".connected",
+    });
+
+    $("li").tooltip({
+        placement: 'top'
+    });
+
+    $("[data-export]").click(exportBookmarks);
 }
 
 function setCollapse(id) {
-	$(".panel-"+id).click(function(event) {
-		var tgt = $(event.target);
-		if(tgt.hasClass("panel-opened")) {
-			tgt.removeClass("panel-opened");
-			tgt.addClass("panel-closed");
-			tgt.next().hide();
-		}
-		else {
-			tgt.addClass("panel-opened");
-			tgt.removeClass("panel-closed");
-			tgt.next().show();
-		}
-	});
+    $(".panel-" + id).click(function(event) {
+        var tgt = $(event.target);
+        if (tgt.hasClass("panel-opened")) {
+            tgt.removeClass("panel-opened");
+            tgt.addClass("panel-closed");
+            tgt.next().hide();
+        } else {
+            tgt.addClass("panel-opened");
+            tgt.removeClass("panel-closed");
+            tgt.next().show();
+        }
+    });
+}
+
+function addWindow(w, el) {
+    var collapseId;
+    var panel = '';
+    if (w) {
+        collapseId = w.id;
+        panel = windowTemplate({ wId: w.id, wTitle: 'Window - ' + w.id, tabs: w.tabs });
+    } else {
+        collapseId = winCounter++;
+        panel = windowTemplate({ wId: 0, wTitle: 'New Window', tabs: [] });
+    }
+    $(el).append(panel);
+    setCollapse(collapseId);
 }
 
 function newWin(evt) {
-	addWindow(null,$("#results"));
-	$(".tabli").sortable({
-	  connectWith: ".connected"
-	});
+    addWindow(null, $("#results"));
+    $(".tabli").sortable({
+        connectWith: ".connected"
+    });
 }
 
-function addWindow(w,el) {
-	var collapseId;
-	var panel = '<div class="col-xs-6"><div class="panel panel-info"><div class="panel-heading panel-{0} panel-opened" style="cursor: pointer;">{1}</div>' +
-				'<div class="panel-body"><ul class="connected tabli" data-val="{2}">{3}</ul></div></div></div>';
-	if(w){
-		collapseId = w.id;
-		panel = String.format(panel,w.id,'Window - '+w.id,w.id,getTabs(w.tabs,w.id));
-	}
-	else {
-		collapseId = winCounter++;
-		panel = String.format(panel,collapseId,'New Window',0,'');
-	}
-	$(el).append(panel);
-	setCollapse(collapseId);
+function saveChanges(event) {
+    $("ul").each(function(k, v) {
+        var wid = $(v).attr('data-val');
+        if (wid === "0")
+            chrome.windows.create(null, function(window) {
+                moveTabs(v, window.id);
+            });
+        else
+            moveTabs(v, wid);
+
+    });
+    chrome.windows.getAll({ "populate": true }, processWindows);
+    $("#message").text("Changes Saved");
+    setTimeout(function() { $("#message").text("") }, 2000);
 }
 
-function getTabs(tabsArr,wid) {
-	var tabs = '';
-	for(var i=0;i<tabsArr.length;i++) {
-		var tab = tabsArr[i];
-		var aTab = '<li data-original-title="URL: {0}" data-rel="tooltip" data-tab="{1}" data-window="{2}">{3}</li>';//id,title,url
-		tabs += String.format(aTab,tab.url,tab.id,wid,tab.title);
-	}
-	return tabs;
+function moveTabs(wins, wid) {
+    var tabs = $("li", $(wins));
+    var tabIds = [];
+    for (var i = 0; i < tabs.length; i++) {
+        tabIds.push(parseInt($(tabs[i]).attr('data-tab')));
+    }
+    chrome.tabs.move(tabIds, { "windowId": parseInt(wid), "index": -1 }, null);
 }
 
-function SaveChanges(event) {
-	$("ul").each(function(k,v) { 
-		var wid = $(v).attr('data-val');
-		if(wid === "0")
-			chrome.windows.create(null,function(window) { console.log(v+ " " +window); MoveTabs(v,window.id); });
-		else
-			MoveTabs(v,wid);
-		
-	});
-	chrome.windows.getAll({"populate":true},processWindows);
-	$("#message").text("Changes Saved");
-	setTimeout(function() { $("#message").text("") },2000);
+
+function exportBookmarks(el) {
+    var wid = parseInt($(el.target).attr('data-export'));
+    var window = allWindows.filter(x => x.id === wid);
+    var tabs = window[0].tabs.map(function(obj) {
+        return { url: obj.url, title: obj.title };
+    });
+    var html = bookmarksTemplate({ bookmarks: tabs });
+    download('export.html', html);
+
 }
 
-function MoveTabs(wins,wid) {
-	var tabs = $("li",$(wins));
-	var tabIds = [];
-	for(var i=0;i<tabs.length;i++) {
-		tabIds.push(parseInt($(tabs[i]).attr('data-tab')));
-	}
-	chrome.tabs.move(tabIds, {"windowId":parseInt(wid),"index":-1}, null);
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
